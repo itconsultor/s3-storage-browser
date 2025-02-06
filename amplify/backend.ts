459ad4a41c3b1c75f5b3a45bfb823e6183f2c277
Carlos Tellez <carlos.cuervo@threatx.com>
2025-02-05 19:29:21 -0500

    Added storage configuration and storage component

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,14 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: "us-east-1",
+    bucket_name: "innovative-storage-browser-poc"
+  },
 });
