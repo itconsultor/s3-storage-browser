060aa389c9a11b50846bde0aeeaa5e910b6c9158
Carlos Tellez <carlos.cuervo@threatx.com>
2025-02-05 21:29:09 -0500

    Groups and permissions tests

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -5,8 +5,8 @@ export const storage = defineStorage({
     access: allow => ({
       'test-mailbox/*': [
         allow.guest.to(['get']),
-        allow.authenticated.to(['get', 'write', 'list']),
-        allow.groups(['admin']).to(['read', 'write', 'delete'])
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.groups(['read']).to(['read']),
       ]
     })
   });
